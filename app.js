031357f80b629be997c462d75cecf24b5d8fc702
will <code@willworth.co.uk>
2017-03-29 12:23:07 +0200

    node, bootstrap

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+
+var app = express();
+
+var port = 5000;
+
+app.use(expres.static('public'));
+
+app.get('/', function(req, res){
+   res.send("hello world!") ;
+});   
+
+
+app.listen(port, function(err){
+    console.log('running server on port ' + port);
+})
+
